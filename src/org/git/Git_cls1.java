1a1c7581f3318fdb4afe7d9e8c51d7bdcf2066da
Poojitha7623 <vanajagpn@gmail.com>
2024-07-12 10:19:18 +0530

    add git message

---
+++ src/org/git/Git_cls1.java
@@ -0,0 +1,16 @@
+package org.git;
+
+public class Git_cls1 {
+	
+	private void add() {
+		System.out.println("add");
+	}
+	
+public static void main(String[] args) {
+		
+	Git_cls1 c = new Git_cls1();
+	c.add();
+
+}
+
+}
